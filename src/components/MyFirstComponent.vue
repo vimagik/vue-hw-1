f8ef794187b2c0995f29e07a2e0ba1170e078cbf
Georgiy Rzhavin <vimagik@gmail.com>
2024-11-08 16:34:45 +0300

    Create Vue project on Vite

---
+++ src/components/MyFirstComponent.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from 'vue'
+
+const clickStatus = ref(false)
+
+function clickButton() {
+  clickStatus.value = !clickStatus.value
+}
+</script>
+
+<template>
+  <h1>Первое ДЗ из курса по VUE</h1>
+  <button @click="clickButton">Нажми меня</button>
+  <p v-if="clickStatus">Cюрпиз</p>
+</template>
